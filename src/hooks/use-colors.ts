fa046a3e684a11d681b4e08d488ce1d89bdeaf11
Jake Runzer <jakerunzer@gmail.com>
2020-04-22 11:19:05 +0100

    change up useColors

--- src/hooks/use-colors.ts
+++ src/hooks/use-colors.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Vibrant from "node-vibrant";
+import useAsyncEffect from "use-async-effect";
 
 export type Colors = string[];
 
@@ -9,26 +10,30 @@ export const defaultColors = ["#4BC0C8", "#C779D0", "#FEAC5E"];
 const useColors = (image?: string): Colors | null => {
   const [colors, setColors] = useState<Colors | null>(defaultColors);
 
-  useEffect(() => {
+  useAsyncEffect(async () => {
     if (image == null) {
       return;
     }
 
-    Vibrant.from(image)
-      .getPalette()
-      .then(palette => {
-        if (palette != null) {
-          const cs = [
-            palette?.DarkMuted?.getHex(),
-            palette?.DarkVibrant?.getHex(),
-            palette?.LightVibrant?.getHex(),
-            palette?.LightMuted?.getHex(),
-          ].filter(Boolean) as Colors;
+    const v = new Vibrant(image, {
+      colorCount: 10,
+    });
 
-          setColors(cs);
-        }
-      });
+    const palette = await Vibrant.from(image).getPalette();
+
+    if (palette != null) {
+      const cs = [
+        palette?.DarkMuted?.getHex(),
+        palette?.DarkVibrant?.getHex(),
+        palette?.LightVibrant?.getHex(),
+        palette?.LightMuted?.getHex(),
+      ].filter(Boolean) as Colors;
+
+      setColors(cs);
+    }
   }, [image]);
+
+  useEffect(() => {}, [image]);
 
   return colors;
 };
